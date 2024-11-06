f10958d62f84638f4e99c03996c2659ae4b2ca5f
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-11-06 16:26:21 -0600

    Test: Start test driven development.

--- crawl.js
+++ crawl.js
@@ -1,5 +1,8 @@
-const normalizeURL = () => {
-
+const normalizeURL = (url) => {
+  const myUrl = new URL(url);
+  const hostAndPath = `${myUrl.host}${myUrl.pathname.replace(/\/$/, '')}`;
+  return hostAndPath;
 }
 
+console.log(normalizeURL('https://blog.boot.dev/path'))
 export { normalizeURL }
--- crawl.test.js
+++ crawl.test.js
@@ -1,3 +1,11 @@
-import { test, expect } from "@jest/globals";
+import { describe, test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl";
 
+describe('Normalized urls', () => {
+  test('Return normalized url', () => {
+    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+    expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+    expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+    expect(normalizeURL('http://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+  })
+})
